076fb166e0ccf24513daaf7e832a7e79c70829e9
igorsousa1397 <igor.oa.sous@gmail.com>
2024-09-18 13:35:42 -0300

    Ajuste do .yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,15 @@
-const { defineConfig } = require("cypress")
+  const { defineConfig } = require("cypress")
 
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'https://www.seara.com.br/agendamento-seara/',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+  module.exports = defineConfig({
+    e2e: {
+      baseUrl: 'https://www.seara.com.br/agendamento-seara/',
+      retries: {
+        runMode: 2,   // Tenta reexecutar os testes 2 vezes no modo headless (CI/CD)
+        openMode: 0   // Sem retries no modo interativo (local)
+      },
+      setupNodeEvents(on, config) {
+        // implement node event listeners here
+      },
     },
-  },
-  
-})
+    
+  })
