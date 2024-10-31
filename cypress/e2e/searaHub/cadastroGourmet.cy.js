92db427b13a44c01ce5a5a0ced576a586db07c97
igorsousa1397 <igor.oa.sous@gmail.com>
2024-10-31 18:50:12 -0300

    Ajuste do .yml

--- cypress/e2e/searaHub/cadastroGourmet.cy.js
+++ cypress/e2e/searaHub/cadastroGourmet.cy.js
@@ -1,10 +1,13 @@
 describe('Teste cadastro', () => {
+    before(() => {
+        cy.wait(28000)
+    })
 
     it('Teste', () =>{
 
         for (let i = 0; i < 5; i++) {
             cy.visit('https://www.seara.com.br/seara-momento-gourmet/', { failOnStatusCode: false })
-            cy.wait(1000)
+            cy.wait(28000)
             cy.get(':nth-child(1) > .new-newsletter-form-group > .input').type('teste')
             cy.get(':nth-child(2) > .new-newsletter-form-group > .input').type('teste@teste.com')
             cy.get('.input-phone').type('11999999999')
