23eadf976d40a99a7e3676cf60f02ef8388b9d15
igorsousa1397 <igor.oa.sous@gmail.com>
2024-10-31 18:28:24 -0300

    Ajuste do .yml

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@
       baseUrl: 'https://www.seara.com.br/agendamento-seara/',
       retries: {
         runMode: 2,   // Tenta reexecutar os testes 2 vezes no modo headless (CI/CD)
-        openMode: 1   // Sem retries no modo interativo (local)
+        openMode: 0   // Sem retries no modo interativo (local)
       },
       setupNodeEvents(on, config) {
         // implement node event listeners here
---
+++ cypress/e2e/searaHub/cadastroGourmet.cy.js
@@ -0,0 +1,18 @@
+describe('Teste cadastro', () => {
+
+    it('Teste', () =>{
+
+        for (let i = 0; i < 5; i++) {
+            cy.visit('https://www.seara.com.br/seara-momento-gourmet/')
+            cy.get(':nth-child(1) > .new-newsletter-form-group > .input').type('teste')
+            cy.get(':nth-child(2) > .new-newsletter-form-group > .input').type('teste@teste.com')
+            cy.get('.input-phone').type('11999999999')
+            cy.get('.input-number').type('1997-12-13')
+            cy.get('i').click()
+            cy.get('.new-newsletter-submit').click()
+            cy.wait(5000)
+            cy.get('.new-newsletter-success > a').click()
+        }
+    })
+})
+
