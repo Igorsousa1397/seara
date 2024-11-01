2b5b35d1212f7fa5db997a55027d397cafd94b3c
igorsousa1397 <igor.oa.sous@gmail.com>
2024-11-01 14:14:07 -0300

    Ajuste do .yml

--- cypress/e2e/searaHub/cadastroGourmet.cy.js
+++ cypress/e2e/searaHub/cadastroGourmet.cy.js
@@ -2,15 +2,14 @@ describe('Teste cadastro', () => {
 
     it('Teste', () =>{
 
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 10; i++) {
             cy.visit('https://www.seara.com.br/seara-momento-gourmet/', { failOnStatusCode: false })
-            cy.get(':nth-child(1) > .new-newsletter-form-group > .input', { timeout: 40000 }).type('teste')
-            cy.get(':nth-child(2) > .new-newsletter-form-group > .input').type('teste@teste.com')
-            cy.get('.input-phone').type('11999999999')
-            cy.get('.input-number').type('13-12-1997')
+            cy.get(':nth-child(1) > .new-newsletter-form-group > .input', { timeout: 40000 }).type('teste', { delay: 0 })
+            cy.get(':nth-child(2) > .new-newsletter-form-group > .input').type('teste@teste.com', { delay: 0 })
+            cy.get('.input-phone').type('11999999999', { delay: 0 })
+            cy.get('.input-number').type('13-12-1997', { delay: 0 })
             cy.get('i').click()
             cy.get('.new-newsletter-submit').click()
-            cy.wait(3000)
             cy.get('.new-newsletter-success > a').click({force: true})
         }
     })
